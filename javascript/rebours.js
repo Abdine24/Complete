20b30af60c6439e64b6a92598aa18ba5eda5f03b
Abdine24 <abdinenikiema79@gmail.com>
2024-01-05 23:26:09 +0000

    slide added

--- javascript/rebours.js
+++ javascript/rebours.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("Dec 28, 2023 20:46:00").getTime();
+var countDownDate = new Date("Jan 08, 2024 23:59:59").getTime();
 
 // Mettre à jour le compte à rebours toutes les secondes
 var x = setInterval(function() {
